9e0c61d2ffc1b9e5779da4c3188d2a5561935ef5
Orkhn <orxan.ahadov@gmail.com>
2021-09-08 20:59:31 +0400

    add header element

---
+++ components/HeaderIcon.js
@@ -0,0 +1,13 @@
+const HeaderIcon = ({ Icon, active }) => {
+  return (
+    <div className="group flex items-center cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-blue-500">
+      <Icon
+        className={`${
+          active && "text-blue-500"
+        } h-5 text-center sm:h-7 mx-auto group-hover:text-blue-500 text-gray-500`}
+      />
+    </div>
+  );
+};
+
+export default HeaderIcon;
